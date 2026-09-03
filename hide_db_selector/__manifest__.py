663171dc160fb5a82a4afab86a7a8ab5ceecc4c1
Ahmmedmohmed <am2157678@gmail.com>
2026-09-03 17:36:58 +0300

    up

---
+++ hide_db_selector/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hide Database Selector',
+    'version': '17.0.1.0.0',
+    'category': 'Technical',
+    'summary': 'إخفاء حقل قاعدة البيانات من صفحة تسجيل الدخول',
+    'author': 'Custom',
+    'depends': ['web'],
+    'data': [
+        'views/login_template.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'hide_db_selector/static/src/css/hide_db.css',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
